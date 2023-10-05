1595dd961660030c9f835dd1068eef1d17ef0b29
ReinaldoMantovani <reinaldotha88@gmail.com>
2023-10-05 17:08:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className="text-3xl text-red-700  text-center">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
