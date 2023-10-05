4e3c18aa7fbe6ac297ebd5b2d21583fbe567ebdc
ReinaldoMantovani <reinaldotha88@gmail.com>
2023-10-05 17:24:30 -0300

    add boxicons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
+import React from 'react';
+import './App.css';
+import 'boxicons';
 
-import './App.css'
-
-function App() {
-  
-
+const App = () => {
   return (
     <div>
       <h1 className="text-3xl text-red-700  text-center">
-      Hello world!
+      Hello world! 
+     
     </h1>
     </div>
   )
